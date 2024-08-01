d88008410ef367846074379bc98419848599c90a
admin113az <140032139+admin113az@users.noreply.github.com>
2024-08-01 15:28:56 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+const hamburger = document.getElementById('hamburger');
+    const menu = document.querySelector('.header-menu');
+    
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('active');
+=======
+const hamburger = document.getElementById('hamburger');
+    const menu = document.querySelector('.header-menu');
+    
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('active');
+>>>>>>> 55c09f693a1dc8427bd661b1f5e1d0cec18fa642
+    });
